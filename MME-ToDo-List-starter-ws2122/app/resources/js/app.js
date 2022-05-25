9237ffc675ea74e418186ae7ece36b983f4a0be4
Bam33751 <76065789+Bam33751@users.noreply.github.com>
2022-05-25 12:22:23 +0200

    Code from practice session

--- MME-ToDo-List-starter-ws2122/app/resources/js/app.js
+++ MME-ToDo-List-starter-ws2122/app/resources/js/app.js
@@ -1,3 +1,21 @@
-/*eslint-env browser */
+import Task from "./task.js";
+import TaskView from "./taskView.js";
 
-console.log("Hello World");
+var taskListEl;
+
+function init(){
+    console.log("Hello World");
+
+
+    taskListEl = document.querySelector(".task-list");
+    let task = new Task();
+    let taskView = new TaskView(task);
+    
+    let el = taskView.getElement();
+    console.log(el);
+    taskListEl.append(el);
+
+}
+
+init();
+
--- MME-ToDo-List-starter-ws2122/app/resources/js/task.js
+++ MME-ToDo-List-starter-ws2122/app/resources/js/task.js
@@ -1,11 +1,21 @@
 const NEW_TASK_TEXT = "New Task";
 
 class Task{
-    constructor(description = NEW_TASK_TEXT, id = Date.now().toString.trim(), status = false){
+    constructor(description = NEW_TASK_TEXT, id = Date.now().toString().trim(), status = false){
         this.description = description;
         this.id = id;
         this.status = status;
+    }
 
+    updateDescription(description){
+        this.description = this.description;
+        return this.description;
+    }
 
+    toggleStatus(){
+        this.status = !this.status;
+        return this.status;
     }
 }
+
+export default Task;
